a97f6c2683f66d2ee0d6cf9b69ea397a99d22110
Paul Gear <github@libertysys.com.au>
2012-02-01 13:23:37 +1000

    Honour overrides before original message subject

--- MailArchive/Archive.pm
+++ MailArchive/Archive.pm
@@ -163,7 +163,7 @@ sub process_email ($$$$$)
 	push @toaddr, @ccaddr;
 
 	# validate project number
-	$projnum = check_project_num($projnum, $subject, $subject_override);
+	$projnum = check_project_num($projnum, $subject_override, $subject);
 	unless (defined $projnum) {
 		if ($outgoing) {
 			send_error($msg, "Project number not found in message", \@toaddr);
